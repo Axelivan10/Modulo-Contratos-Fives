3733c500f5cad083cf597f0485cfbbfc72478a6a
Desarrollo Tecnologico <pdesarrollo@TMMXTF.LOCAL>
2021-10-18 16:58:51 -0500

    ENVIAR CORREO 3 FECHAS

--- Proyecto/settings.py
+++ Proyecto/settings.py
@@ -137,8 +137,10 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 from decouple import config
 
-EMAIL_HOST = 'smtp.googlemail.com'
-EMAIL_PORT=587
+EMAIL_BACKEND= 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT= 587
 EMAIL_HOST_USER="axelivan02@gmail.com"
 EMAIL_HOST_PASSWORD="axel12345"
-EMAIL_USE_TLS=True    
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False 
--- scripts/scripts.py
+++ scripts/scripts.py
@@ -3,9 +3,12 @@ from background_task import background
 from django.contrib.auth.models import User
 from django.shortcuts import render
 from Contratos.models import *
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, date
 from django.conf import settings
 from django.core.mail import send_mail
+
+dicc_vencidos= None
+imprimir_fecha=""
 
 def vista(request):
     notify_user()
@@ -19,17 +22,46 @@ def notify_user():
     
 
 def send_email_contrato():
-    imprimir_fecha=""
+    hoy = date.today()
+    fecha_vencimiento30= hoy + timedelta(days=30)
+    fecha_vencimiento25= hoy + timedelta(days=25)
+    fecha_vencimiento20= hoy + timedelta(days=20)
+    #dicc_vencidos = Dato_contrato.objects.filter(fecha_fin__range=[hoy,fecha_vencimiento])
+    dicc_vencidos = Dato_contrato.objects.filter(fecha_fin__year=fecha_vencimiento30.year,
+                                                 fecha_fin__month=fecha_vencimiento30.month,
+                                                 fecha_fin__day=fecha_vencimiento30.day) | Dato_contrato.objects.filter(fecha_fin__year=fecha_vencimiento25.year,
+                                                 fecha_fin__month=fecha_vencimiento25.month,
+                                                 fecha_fin__day=fecha_vencimiento25.day) | Dato_contrato.objects.filter(fecha_fin__year=fecha_vencimiento20.year,
+                                                 fecha_fin__month=fecha_vencimiento20.month,
+                                                 fecha_fin__day=fecha_vencimiento20.day)
 
+    print(dicc_vencidos)
+    if dicc_vencidos.exists():
+        #imprimir_vencidos= str(dicc_vencidos)
+        for contrato in dicc_vencidos:
+            if (contrato.nombre_departamento.count() > 0):
+                for departamento in contrato.nombre_departamento.all(): 
+                    print(departamento.nombre)
+                    for correo in contrato.nombre_departamento.all():
+                       #depa_nombre = correo.correo_encargado
+                        print(correo.correo_encargado)
 
-    hoy = datetime.today()
-    fecha_vencimiento= hoy + timedelta(days=30)
-    dicc_vencidos = Dato_contrato.objects.filter(fecha_fin__range=[hoy,fecha_vencimiento])
+                    imprimir_fecha= str(contrato.fecha_fin)
+                    send_mail(
+                    'RECORDATORIO CONTRATOS THE FIVES BEACH',
+                    f'A quien corresponda,\n \nSe le recuerda que el contrato de proveedores de The Fives con los siguientes datos: \n \nNombre: {contrato.nombre_contrato.upper()} \nDepartamento: {departamento.nombre.upper()} \nHotel: {contrato.empresa.nombre_hotel.upper()} \nProveedor: {contrato.proveedor.nombre_proveedor.upper()} \nFecha de vencimiento: {imprimir_fecha} \n \nSaludos condiales, \nThe Fives'
+                    ,
+                    settings.EMAIL_HOST_USER,
+                    [correo.correo_encargado],
+                    fail_silently=False
+                    )
+                    
+                    
+    else:
+        print("NO TIENES CONTRATOS POR VENCER ")
 
-    for impresion in dicc_vencidos:
-            print(impresion)
-
-
+'''
+def contrato_check():
     if dicc_vencidos.exists():
         #imprimir_vencidos= str(dicc_vencidos)
         for contrato in dicc_vencidos:
@@ -40,6 +72,7 @@ def send_email_contrato():
                     for correo in contrato.nombre_departamento.all():
                        #depa_nombre = correo.correo_encargado
                         print(correo.correo_encargado)
+
                         
                     imprimir_fecha= str(contrato.fecha_fin)
                     send_mail(
@@ -57,44 +90,18 @@ def send_email_contrato():
         
 
 
+
 '''
-def send_email_contrato(user_id):
-    nombre_contratos = Dato_contrato.objects.all()
-    fiscal = Dato_contrato.objects.filter(id=9)
-    email= Departamento.objects.filter(id=user_id)
-    email2=  Departamento.objects.all()
-    correo=""
-    for nuevoemail  in email:
-        correo= nuevoemail.correo_encargado
-
-    print(nombre_contratos)
-    print(email)
-    print(fiscal)
-    print(email2)
-    for contratos in nombre_contratos:
-        print(contratos.empresa.nombre_hotel)
-    print(correo)
-    str(correo)
-    print(correo)
-    send_mail(  
-    'RECORDATORIO CONTRATOS',
-    'Buen día, le recuerdo que el contrato esta a punto de expirar',
-    settings.EMAIL_HOST_USER,
-    [correo],
-    fail_silently=False
-    )
 
 
 
-
-    for contratos in nombre_contratos:
-        print(contratos.nombre_hotel)
-
-
-  if Departamento.objects.filter(name=Departamento.nombre).count():
-
-for dicc_vencidos in Dato_contrato.objects.filter(fecha_fin__range=[hoy,fecha_vencimiento]):
-        print (dicc_vencidos)
-        lista.append(dicc_vencidos)
-    '''
-
+ #'RECORDATORIO CONTRATOS THE FIVES BEACH',
+  #                  f'''A quien corresponda, se le recuerda que el contrato de proveedores de The Fives con los siguientes datos:
+   #                 Nombre:{contrato.nombre_contrato}
+    #                Departamento: {departamento.nombre}
+     #               Hotel:{contrato.empresa.nombre_hotel}
+      #              Proveedor:{contrato.proveedor.nombre_proveedor}
+       #             Fecha de vencimiento:{imprimir_fecha}
+        #            Saludos condiales,
+         #           The Fives
+          #          ''',
